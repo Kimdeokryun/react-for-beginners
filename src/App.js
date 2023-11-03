96d2eb104c8a106d9a44b957771d2a03fdfb8c96
Kimdeokryoun <cjscjsejrxoddl@gmail.com>
2023-11-03 22:25:26 +0900

    staged all files

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import {
 } from "react-router-dom";
 
 function App() {
-  return 
-  <Router basename={process.env.PUBLIC_URL}>
+  return <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/movie/:id" element={<Detail />} />
